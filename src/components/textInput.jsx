5916305e2bddc89cd876459d082144e73dfba0f4
cfccfelipe <cfccfelipe@gmail.com>
2021-11-28 14:47:53 -0500

    Anomalias

--- src/components/textInput.jsx
+++ src/components/textInput.jsx
@@ -1,7 +1,10 @@
-const InputText = props => <input 
-    type={props.type || "text"} 
-    onChange={props.setter} 
-    className="input-text" 
-    value={props.val}  />
-  
- export default InputText
+const InputText = (props) => (
+	<input
+		type={props.type || 'text'}
+		onChange={props.setter}
+		className='input-textt'
+		value={props.val}
+	/>
+);
+
+export default InputText;
